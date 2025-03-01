af90e3f31e41f510e40ad023f4989f250100894c
DeepTrak <authenticjovan@gmail.com>
2025-03-02 02:10:55 +0800

    embeddings generated

--- src/model/RAG-LLM/ollama_embeddings.py
+++ src/model/RAG-LLM/ollama_embeddings.py
@@ -1,26 +1,27 @@
 from langchain_ollama import OllamaEmbeddings
-from langchain_chroma import Chroma
-from dotenv import load_dotenv
-from uuid import uuid4
-from langchain_core.documents import Document
-import os
+from langchain_community.document_loaders.csv_loader import CSVLoader  # CSV
+import faiss
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
 
-load_dotenv()
-
-def load_database(doc_list): 
+# Headers.csv use csvloader()
+def load_database(csv_file): 
     
     embeddings = OllamaEmbeddings(
-        model="mxbai-embed-large",
+        model="mxbai-embed-large"
     )
 
-    vector_store = Chroma(
-        collection_name="example_collection",
-        embedding_function=embeddings,
-        persist_directory=os.getenv("DB_DUMP_DIRECTORY")
-    )
+    embeddings = OllamaEmbeddings()
+    index = faiss.IndexFlatL2(len(OllamaEmbeddings().embed_query(" ")))
+    vector_store = FAISS(
+        embedding_function=OllamaEmbeddings(),
+        index=index,
+        docstore=InMemoryDocstore(),
+        index_to_docstore_id={}
+        )
+    
+    # Add csv documents into vectorstore
+    vector_store.add_documents(documents=csv_file)
 
-    uuids = [str(uuid4()) for _ in range(len(doc_list))]     
+    return vector_store
 
-    vector_store.add_documents(documents=doc_list, ids=uuids)
-
-    return doc_list[2]
